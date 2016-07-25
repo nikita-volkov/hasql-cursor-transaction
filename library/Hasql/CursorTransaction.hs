26e71e88bcd694deb3941b145b5f20ac5b3898d0
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-07-25 16:14:21 +0300

    Rearrange Specs

--- library/Hasql/CursorTransaction.hs
+++ library/Hasql/CursorTransaction.hs
@@ -1,12 +1,24 @@
 module Hasql.CursorTransaction
 (
-  CursorTransaction,
-  Cursor,
-  withCursor,
-  fetchBatch,
-  transaction,
+  -- * Types
+  A.CursorTransaction,
+  A.Cursor,
+  B.BatchSize,
+  B.EncodedParams,
+  -- * Cursor Transactions
+  A.withCursor,
+  A.fetchBatch,
+  A.transaction,
+  -- * Batch Sizes
+  B.batchSize_10,
+  B.batchSize_100,
+  B.batchSize_1000,
+  B.batchSize_10000,
+  -- * Encoded Params
+  B.encodedParams,
 )
 where
 
-import Hasql.CursorTransaction.Private.CursorTransaction
+import qualified Hasql.CursorTransaction.Private.CursorTransaction as A
+import qualified Hasql.CursorTransaction.Private.Specs as B
 
