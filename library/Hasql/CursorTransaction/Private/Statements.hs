57a174bba82830d467c3bd141ab29dcfdd92e486
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-01-07 01:57:14 +0300

    Migrate from ByteString to Text

--- library/Hasql/CursorTransaction/Private/Statements.hs
+++ library/Hasql/CursorTransaction/Private/Statements.hs
@@ -1,37 +1,37 @@
 module Hasql.CursorTransaction.Private.Statements where
 
-import Data.Text (Text, pack)
-import Data.Text.Encoding (decodeUtf8)
+import qualified Data.Text as Text
 import Hasql.CursorTransaction.Private.Prelude
 import qualified Hasql.CursorTransaction.Private.Specs as F
 import qualified Hasql.Decoders as C
 import qualified Hasql.Encoders as B
 import Hasql.Statement
 
-declareCursor :: ByteString -> ByteString -> B.Params a -> Statement a ()
+declareCursor :: Text -> Text -> B.Params a -> Statement a ()
 declareCursor name sql encoder =
   unpreparable sql' encoder C.noResult
   where
     sql' =
       "DECLARE "
-        <> decodeUtf8 name
+        <> name
         <> " NO SCROLL CURSOR FOR "
-        <> decodeUtf8 sql
+        <> sql
 
-closeCursor :: ByteString -> Statement () ()
+closeCursor :: Text -> Statement () ()
 closeCursor name =
   preparable sql B.noParams C.noResult
   where
     sql =
-      "CLOSE " <> decodeUtf8 name 
-fetchFromCursor :: ByteString -> F.BatchSize -> C.Result result -> Statement () result
+      "CLOSE " <> name
+
+fetchFromCursor :: Text -> F.BatchSize -> C.Result result -> Statement () result
 fetchFromCursor name (F.BatchSize batchSize) decoder =
   preparable sql encoder decoder
   where
     sql =
       "FETCH FORWARD "
-        <> pack (show batchSize)
+        <> Text.pack (show batchSize)
         <> " FROM "
-        <> decodeUtf8 name
+        <> name
     encoder =
       B.noParams
--- library/Hasql/CursorTransaction/Private/Transactions.hs
+++ library/Hasql/CursorTransaction/Private/Transactions.hs
@@ -7,14 +7,14 @@ import qualified Hasql.Decoders as E
 import qualified Hasql.Encoders as F
 import qualified Hasql.Transaction as C
 
-declareCursor :: ByteString -> ByteString -> F.Params params -> params -> C.Transaction ()
+declareCursor :: Text -> Text -> F.Params params -> params -> C.Transaction ()
 declareCursor cursorName template paramsEncoder params =
   C.statement params (A.declareCursor cursorName template paramsEncoder)
 
-closeCursor :: ByteString -> C.Transaction ()
+closeCursor :: Text -> C.Transaction ()
 closeCursor cursorName =
   C.statement () (A.closeCursor cursorName)
 
-fetchFromCursor :: ByteString -> B.BatchSize -> E.Result result -> C.Transaction result
+fetchFromCursor :: Text -> B.BatchSize -> E.Result result -> C.Transaction result
 fetchFromCursor cursorName batchSize decoder =
   C.statement () (A.fetchFromCursor cursorName batchSize decoder)
